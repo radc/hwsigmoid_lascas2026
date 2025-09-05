5918ec2d34ec33b1f7dfb9b2a2cf4e9b9a50ad2f
Ruhan@Arendt <ruhanconceicao@gmail.com>
2025-09-05 16:38:58 -0300

    DCVC-RT

---
+++ DCVC/launch.sh
@@ -0,0 +1,7 @@
+python test_video.py \
+    --model_path_i ./checkpoints/cvpr2025_image.pth.tar \
+    --model_path_p ./checkpoints/cvpr2025_video.pth.tar \
+    --rate_num 4 --test_config ./dataset_config_tcsvt_hevcb.json \
+    --cuda 1 -w 1 --write_stream 1 --force_zero_thres 0.12 \
+    --output_path outputs_neurips_peng/anchor_fp.json \
+    --force_intra_period -1 --reset_interval 64 --force_frame_num -1 --check_existing 0
